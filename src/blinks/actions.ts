ae8e454ccb0c5c37fac71e7868404820e81a8972
kox <garsanzi@gmail.com>
2024-08-21 01:10:51 +0200

    fixing action root uri

--- src/blinks/actions.ts
+++ src/blinks/actions.ts
@@ -2,7 +2,7 @@ export const actions = {
   rules: [
     {
       pathPattern: '/buy-me-a-coffee/**',
-      apiPath: 'http://www.cognis.es/api/actions/buy-me-a-coffee/**',
+      apiPath: 'https://api.cognis.es/api/actions/buy-me-a-coffee/**',
     },
   ],
 };
